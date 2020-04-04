9acb85b1eba9e8f7637dcca9deb6e2ff0895d33b
Flip437 <philippediollot@hotmail.fr>
2020-04-04 13:02:24 +0200

    admin template ok

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,28 @@
+class AdminController < ApplicationController
+
+    def index
+        @gigs = Gig.all
+    end
+    
+    def new
+        @gig = Gig.new
+    end
+    
+    def create
+    end
+    
+    def show
+        @gig = Gig.find(params[:id])
+    end
+    
+    def edit
+        @gig = Gig.find(params[:id])
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   root "home#index"
 
   resources :gig
+  resources :admin
 
 end
