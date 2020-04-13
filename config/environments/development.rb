c6c6bb1decf90a7042ac2b3e1b44b98394faea58
Flip437 <philippediollot@hotmail.fr>
2020-04-13 15:17:07 +0200

    font mail OK

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,5 +63,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.asset_host = 'http://localhost:3000'
 
 end
