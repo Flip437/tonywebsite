a094f89919e22e166f3282c7f91b82b3b11dfdba
Flip437 <philippediollot@hotmail.fr>
2020-04-16 16:42:21 +0200

    adding default url option to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.ico]
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.default_url_options = { host: "https://staging-anthonysiteweb.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "http://www.anthonyrivera-barython.fr" }
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 end
