dfa903b36143e97f73f8ab735e9bdb71565066f6
Flip437 <philippediollot@hotmail.fr>
2020-05-07 12:57:00 +0200

    jsquey on rel attributs working

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
+
+  $('a[rel="lightbox[1]"]').attr('rel','lightbox[2]');
+
+
   $(".fancybox").fancybox({
-        openEffect: "none",
-        closeEffect: "none"
-    });
+    openEffect: "none",
+    closeEffect: "none"
+  });
     
-    $(".zoom").hover(function(){
-		
-		$(this).addClass('transition');
-	}, function(){
+  $(".zoom").hover(function(){
+  
+    $(this).addClass('transition');
+  }, function(){
         
-		$(this).removeClass('transition');
-	});
+    $(this).removeClass('transition');
+  });
+
 });
