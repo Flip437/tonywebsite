6ff4182cb5549c6139cb91ea3fcc2de09985954a
Flip437 <philippediollot@hotmail.fr>
2020-05-12 19:56:08 +0200

    adding thumbnal method to video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,9 @@
 class Video < ApplicationRecord
     belongs_to :gallery, optional: true
+
+    def thumbnail(url)
+        equalindex = url.index("=")
+        id = url[equalindex+1, url.length - equalindex]
+        thumbnailurl = "https://img.youtube.com/vi/" + id + "/default.jpg"
+    end
 end
