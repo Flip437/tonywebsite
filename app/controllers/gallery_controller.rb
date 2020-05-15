d544c0b6a044de4c9ba0a4d58d39facf46691719
Flip437 <philippediollot@hotmail.fr>
2020-05-15 13:16:57 +0200

    remove signature url

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -7,7 +7,7 @@ class GalleryController < ApplicationController
     set_meta_tags og: {
       title:    @page_title,
       type:     'article',
-      image: "http://d1q8yhpgns3eh4.cloudfront.net/assets/core-img/signature_small-034946ef865e1e37df3c663e1fef606b92d929d363f96bee4f814388dc3229c5.png",
+      image: "",
       url:      'http://www.anthonyrivera-baryton.fr/gallery',
       site_name: 'Anthony Rivera'
     }
