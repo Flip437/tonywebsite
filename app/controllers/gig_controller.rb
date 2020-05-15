a2297a40487d96522b78e49b828e214788bd14ae
Flip437 <philippediollot@hotmail.fr>
2020-05-15 14:27:37 +0200

    adding class del btn

--- app/controllers/gig_controller.rb
+++ app/controllers/gig_controller.rb
@@ -7,7 +7,7 @@ class GigController < ApplicationController
     set_meta_tags og: {
       title:    @page_title,
       type:     'article',
-      image: "",
+      image: "http://d1q8yhpgns3eh4.cloudfront.net/assets/core-img/signature_small-034946ef865e1e37df3c663e1fef606b92d929d363f96bee4f814388dc3229c5.png",
       url:      'http://www.anthonyrivera-baryton.fr/gig/',
       site_name: 'Anthony Rivera'
     }
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
             title:    'Anthony Rivera - Opera singer',
             type:     'website',
             description: @page_description,
-            image: "",
+            image: "http://d1q8yhpgns3eh4.cloudfront.net/assets/core-img/signature_small-034946ef865e1e37df3c663e1fef606b92d929d363f96bee4f814388dc3229c5.png",
             url:      'http://www.anthonyrivera-baryton.fr/',
             site_name: 'Anthony Rivera'
           }
