a7ad4ac9b872b1592e51d441812691bd30b47364
Flip437 <philippediollot@hotmail.fr>
2020-05-27 11:12:42 +0200

    adding og img from other website

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
             title:    'Anthony Rivera - Opera singer',
             type:     'website',
             description: @page_description,
-            image: "https://github.com/Flip437/tonywebsite/blob/584c64b09072a2e2ade0df359620b62b2e4c400c/app/assets/images/bg-img/anthony_avatar.jpeg",
+            image: "https://static.wixstatic.com/media/24fa41_4783aa08dd5e44c0ad068630155f0f29.jpg/v1/fill/w_516,h_762,al_c,q_85,usm_0.66_1.00_0.01/24fa41_4783aa08dd5e44c0ad068630155f0f29.webp",
             url:      'http://www.anthonyrivera-baryton.fr/',
             site_name: 'Anthony Rivera'
           }
